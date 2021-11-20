c0e8ab9bbdfef57eacfe22ed9779ab0c2b491996
astock87 <astock87@gmail.com>
2021-11-20 16:16:32 -0500

    adding more coding exercises

---
+++ day1_start.py
@@ -0,0 +1,13 @@
+#Write code that switches the outputs for a and b
+#Don't change the code below:
+a = input("a: ")
+b = input("b: ")
+#Don't change the code above.
+
+c = a
+a = b
+b = c
+
+#Don't change code below:
+print("a: " + a)
+print("b: " + b)
