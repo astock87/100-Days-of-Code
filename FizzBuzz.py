b60e79fadadb37493fdaad4afa2c73e1e17cac22
Amanda <93019898+astock87@users.noreply.github.com>
2022-05-05 16:33:19 -0400

    changed some formatting

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,9 +2,9 @@
 
 # When the number is divisible by 3 then instead of printing the number it should print "Fizz".
 
-# `When the number is divisible by 5, then instead of printing the number it should print "Buzz".` 
+# When the number is divisible by 5, then instead of printing the number it should print "Buzz".` 
 
-#   `And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz"`
+# And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz"`
 
 #Write your code below this row 👇
 
