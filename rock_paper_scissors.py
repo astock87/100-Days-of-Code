e2f8e3f215703562d60b3237b598ee7715969652
Amanda <astock87@gmaill.com>
2022-05-05 19:34:14 -0400

    fixed formatting

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -25,9 +27,7 @@ scissors = '''
 ---.__(___)
 '''
 
-#Write your code below this line 👇
-
-import random
+# Write your code below this line 👇
 
 # Rock beats Scissors, Paper beats Rock, Scissors beat Paper
 # 0 for Rock
@@ -39,22 +39,21 @@ game_images = [rock, paper, scissors]
 player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
 if player_choice >= 3 or player_choice < 0:
-  print("Invalid choice, you lose.")
+    print("Invalid choice, you lose.")
 else:
-  print(game_images[player_choice])
+    print(game_images[player_choice])
 
-  computer_choice = random.randint(0, 2)
-  print("The computer chooses: ")
-  print(game_images[computer_choice])
+    computer_choice = random.randint(0, 2)
+    print("The computer chooses: ")
+    print(game_images[computer_choice])
 
-
-  if player_choice == 0 and computer_choice == 2:
-    print("You win!")
-  elif player_choice == 2 and computer_choice == 0:
-    print("You lose")
-  elif player_choice > computer_choice:
-    print("You win!")
-  elif computer_choice > player_choice:
-    print("You lose")
-  elif player_choice == computer_choice:
-    print("It's a draw")
+    if player_choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif player_choice == 2 and computer_choice == 0:
+        print("You lose")
+    elif player_choice > computer_choice:
+        print("You win!")
+    elif computer_choice > player_choice:
+        print("You lose")
+    elif player_choice == computer_choice:
+        print("It's a draw")
