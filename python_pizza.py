ecf615b0d5dca46b12d60da085526297b57afac4
Amanda <amstock287@gmail.com>
2025-09-03 23:05:45 -0400

    updated file

--- python_pizza.py
+++ python_pizza.py
@@ -1,8 +1,8 @@
 # 🚨 Don't change the code below 👇
 print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M, or L :").lower()
-add_pepperoni = input("Do you want pepperoni? Y or N :").lower()
-extra_cheese = input("Do you want extra cheese? Y or N : ").lower()
+size = input("What size pizza do you want? S, M, or L :")
+add_pepperoni = input("Do you want pepperoni? Y or N :")
+extra_cheese = input("Do you want extra cheese? Y or N : ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
