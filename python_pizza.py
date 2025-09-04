9ff2cd13a05b59d078dff7574e3f2d9be468e22c
Amanda <amstock287@gmail.com>
2025-09-03 23:02:15 -0400

    update code and it works now :D

--- python_pizza.py
+++ python_pizza.py
@@ -1,27 +1,36 @@
 # 🚨 Don't change the code below 👇
 print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M, or L ")
-add_pepperoni = input("Do you want pepperoni? Y or N ")
-extra_cheese = input("Do you want extra cheese? Y or N ")
+size = input("What size pizza do you want? S, M, or L :")
+add_pepperoni = input("Do you want pepperoni? Y or N :")
+extra_cheese = input("Do you want extra cheese? Y or N : ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-#Small Pizza: $15, Medium Pizza: $20, Large Pizza: $25
-#Pepperoni for Small Pizza: +$2, Pepperoni for Medium or Large Pizza: +$3
-#Extra cheese for any size pizza: + $1
+#todo: how much based on size selection: Small Pizza: $15, Medium Pizza: $20, Large Pizza: $25
+
 bill = 0
 
 if size == "S":
-  bill = 15
-  if add_pepperoni == "Y":
-    bill += 2
-if size == "M":
-  bill = 20
-if size == "L":
-  bill = 25     
+  bill += 15
+elif size == "M":
+  bill += 20
+elif size == "L":
+  bill += 25
+else:
+  print("You typed the wrong input")
+
+#todo: how much to add if they want pepperoni: #Pepperoni for Small Pizza: +$2, Pepperoni for Medium or Large Pizza: +$3
+
 if add_pepperoni == "Y":
-  bill += 3
+    if size == "S":
+       bill += 2
+    else:
+       bill += 3
+
+#todo: how much to add if they want extra cheese: #Extra cheese for any size pizza: + $1
+
 if extra_cheese == "Y":
   bill += 1
 
 print(f"Your final bill is: ${bill}.")
+
