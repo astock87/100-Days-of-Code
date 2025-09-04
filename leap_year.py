8af494104ea3a9adac5498b51a2368ffc457e73f
Amanda <amstock287@gmail.com>
2025-09-03 22:12:18 -0400

    updated input

--- leap_year.py
+++ leap_year.py
@@ -9,7 +9,7 @@ if year % 4 == 0:
     if year % 400 == 0:
       print("Leap year.")
     else:
-      print("Not leap year.")
+      print("Not a leap year.")
   else:
     print("Leap year.")
 else:
