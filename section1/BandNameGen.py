46fbac6e916f60f3508d4ec23151e7ba645f5ebe
Amanda <amstock287@gmail.com>
2025-09-09 15:25:06 -0400

    added folders, created band name generator

---
+++ section1/BandNameGen.py
@@ -0,0 +1,15 @@
+# Band Name Generator
+
+print("Welcome to the Awesome Band Name Generator!")
+
+# ask for user input
+
+city = input("What city did you grow up in?\n")
+
+# ask for the name of their pet
+
+pet = input("What is the name of your pet?\n")
+
+# string concatenation to combine inputs
+
+print("Your band name could be: " + city + " " + pet)
